9ba113bb9bda4183ea663e7a1e3b7e2869aa2c18
Moritz Schmidt <fusselwurm@gmail.com>
2015-04-10 01:14:47 +0200

    incr version, add author info

--- ar3play/config.cpp
+++ ar3play/config.cpp
@@ -1,4 +1,4 @@
-class CfgPatches 
+class CfgPatches
 {
 	class ar3play
 	{
@@ -6,15 +6,20 @@ class CfgPatches
 		weapons[] = { };
 		requiredVersion = 0.100000;
 		requiredAddons[] = {"CBA_Extended_EventHandlers"};
-		version = "1";
+		version = "2";
 		projectName = "ar3play";
+		author[] = {
+			"Fusselwurm <fusselwurm@gmail.com>",
+			"ZiP"
+		};
+		authorUrl = "https://github.com/gruppe-adler/ar3play-addon";
 	};
 };
 
-class Extended_PostInit_EventHandlers 
+class Extended_PostInit_EventHandlers
 {
-    class ar3play
+	class ar3play
 	{
-        Init = "call compile preprocessFileLineNumbers '\ar3play\init.sqf'";
+		Init = "call compile preprocessFileLineNumbers '\ar3play\init.sqf'";
 	};
 };
